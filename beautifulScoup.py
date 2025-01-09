5c747ed06a2ae356fe25d315090450d03db876a8
Ashen Madhushanka <161613675+amliyanage@users.noreply.github.com>
2025-01-09 12:05:19 +0530

    Refactor beautifulScoup.py to fetch HTML content from a URL using requests and extract the title

--- beautifulScoup.py
+++ beautifulScoup.py
@@ -1,22 +1,13 @@
 from bs4 import BeautifulSoup
+import requests
 
-html_doc = """ <html><head><title>The Dormouse's story</title></head>
-                <body>
-                <p class="title"><b>The Dormouse's story</b></p>
-                <p class="story">Once upon a time there were three little sisters; and their names were
-                <a href="http://example.com/elsie&quot; class="sister" id="link1">Elsie</a>,
-                <a href="http://example.com/lacie&quot; class="sister" id="link2">Lacie</a> and
-                <a href="http://example.com/tillie&quot; class="sister" id="link3">Tillie</a>;
-                and they lived at the bottom of a well.</p>
-                <p class="story">...</p> """ 
+url = 'http://example.com'
+response = requests.get(url)
 
-soup = BeautifulSoup(html_doc,'html.parser')
-# convert html to string
-print(soup.text)
+if response.status_code == 200 :
+    scoup = BeautifulSoup(response.text,'html.parser')
+    title = scoup.h1
+    print(title.text)
 
-# get first p tag
-print(soup.p)
-# get first a tag
-print(soup.a)
-# get fist p tag
-print(soup.find('p'))
+else :
+    print("Error")
